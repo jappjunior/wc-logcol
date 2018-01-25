2a307022ddd90cce8e1108fb8d77001bccd9e48e
jappjunior <jappjunior@yahoo.com.br>
2018-01-25 12:08:40 -0200

    corrido bug que não aparecia o método no painel de entregas

--- wc-logcol.php
+++ wc-logcol.php
@@ -2,12 +2,12 @@
 /*
  * Plugin Name: Transportadora LogCol
  * Description: Integra a tabela de frete da Kaiowá do Brasil nos métodos de entrega do Woocommerce
- * Plugin URI:           https://github.com/logcol
+ * Plugin URI:           https://github.com/jappjunior/wc-logcol
  * Author:               José Pimenta
  * Author URI:           https://kaiowadobrasil.com.br
  * Version:              1.0
  * License:              GPLv2 or later
- * Text Domain:          logcol
+ * Text Domain:          wc-logcol
  * Domain Path:          /languages
  * WC requires at least: 3.0.0
  * WC tested up to:      3.2.0
@@ -29,7 +29,7 @@ register_activation_hook( __FILE__, 'r_activate_plugin');
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
  
     //função que inclui as configuraões do método de postagem antes da classe ser instsanciada
-    function logcol_shipping_method() {
+    function logcol_shipping_method_init() {
         if ( ! class_exists( 'LogCol_Shipping_Method' ) ) {
             class LogCol_Shipping_Method extends WC_Shipping_Method {
                 /**
@@ -39,19 +39,19 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                  * @return void
                  */
                 public function __construct( $instance_id = 0 ) {
-                
-                      $this->id                 = 'logcol';
+
+                      $this->id                 = 'logcol_shipping_method'; // Id for your shipping method. Should be uunique.
                       $this->instance_id        = absint( $instance_id );
                       
-                      $this->method_title       = __( 'LogCol Shipping', 'logcol' );
-                      $this->method_description = __( 'Tabela da LogCol para Kaiowá do Brasil', 'logcol' );
+                      $this->method_title       = __( 'LogCol Shipping', 'wc-logcol' );
+                      $this->method_description = __( 'Tabela da LogCol para Kaiowá do Brasil', 'wc-logcol' );
                       
                       $this->supports           = array(
                             'shipping-zones',
                             'instance-settings'
                             );
 
-                      $this->init();
+                      $this->init();  
                 }
 
                 /**
@@ -62,8 +62,9 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
                 */
                 function init() {
                   // Load the settings API
+                  $this->init_settings(); //cometario teste
                   $this->init_form_fields(); 
-                  $this->init_settings(); 
+
 
                   // Define user set variables.
                   $this->enabled    = $this->get_option('enabled');
@@ -83,38 +84,42 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
  
                     $this->instance_form_fields = array(
                           'title' => array(
-                              'title'       => __( 'Title', 'logcol' ),
+                              'title'       => __( 'Title', 'wc-logcol' ),
                               'type'        => 'text',
-                              'description' => __( 'Title to be display on site', 'logcol' ),
+                              'description' => __( 'Title to be display on site', 'wc-logcol' ),
                               'default'     => $this->method_title,
                               'desc_tip'    => true,
                               ),
 
                         'requires' => array(
-                              'title'   => __( 'Free shipping requires...', 'logcol' ),
+                              'title'   => __( 'Free shipping requires...', 'wc-logcol' ),
                               'type'    => 'select',
                               'class'   => 'wc-enhanced-select',
                               'default' => '',
                               'options' => array(
-                                  ''           => __( 'N/A', 'logcol' ),
-                                  'coupon'     => __( 'A valid free shipping coupon', 'logcol' ),
-                                  'min_amount' => __( 'A minimum order amount', 'logcol' ),
-                                  'either'     => __( 'A minimum order amount OR a coupon', 'logcol' ),
-                                  'both'       => __( 'A minimum order amount AND a coupon', 'logcol' ),
+                                  ''           => __( 'N/A', 'wc-logcol' ),
+                                  'coupon'     => __( 'A valid free shipping coupon', 'wc-logcol' ),
+                                  'min_amount' => __( 'A minimum order amount', 'wc-logcol' ),
+                                  'either'     => __( 'A minimum order amount OR a coupon', 'wc-logcol' ),
+                                  'both'       => __( 'A minimum order amount AND a coupon', 'wc-logcol' ),
                                   ),
                               ),
 
                               'min_amount' => array(
-                                  'title'       => __( 'Minimum order amount', 'logcol' ),
+                                  'title'       => __( 'Minimum order amount', 'wc-logcol' ),
                                   'type'        => 'price',
                                   'placeholder' => wc_format_localized_price( 0 ),
-                                  'description' => __( 'Gasto mínimo para usar esse método.', 'logcol' ),
+                                  'description' => __( 'Gasto mínimo para usar esse método.', 'wc-logcol' ),
                                   'default'     => '0',
                                   'desc_tip'    => true,
                               ),
                         );
+              
               $this->form_fields = $this->instance_form_fields;
+
               }
+
+
 
 
                  /** This function is used to calculate the shipping cost. Within this function we can check for weights,
@@ -141,10 +146,10 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
         }
     }
 
-   add_action( 'woocommerce_shipping_init', 'logcol_shipping_method' );
+   add_action( 'woocommerce_shipping_init', 'logcol_shipping_method_init' );
    
    function add_logcol_shipping_method( $methods ) {
-          $methods[] = 'LogCol_Shipping_Method';
+          $methods['logcol_shipping_method'] = 'LogCol_Shipping_Method';
           return $methods;
   }
    
